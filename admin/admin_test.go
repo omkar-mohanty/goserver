e04f4dd5c6e23f22b920dff86c5a001ee04c5e8d
omkar-mohanty <omohanty7professional@gmail.com>
2021-12-17 10:04:05 +0530

    Added loggig feature and admin CLI

--- admin/admin_test.go
+++ admin/admin_test.go
@@ -1,12 +1,16 @@
 package admin
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
 func TestAddLog(t *testing.T) {
-	Log("Added Test entry")
+	Log("Added test entry")
+	for i := 0; i < 10; i++ {
+		Log("Entry:" + fmt.Sprintf("%d", i))
+	}
 	if _, err := os.Open("logs"); err != nil {
 		t.Fatalf("Could not create logs file")
 	}
